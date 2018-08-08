244f5d296291504fc177e10f703dd594dd082d34
Andrew Krotov <akrotoff95@gmail.com>
2018-08-08 20:53:03 +0300

    post-commit for previous commit

--- src/events.js
+++ src/events.js
@@ -1,6 +1,39 @@
 
 document.body.onmouseover = document.body.onmouseout = document.body.onclick = handler;
 
+const ships = document.getElementsByClassName('ship');
+const containers = document.getElementsByClassName('tablecell');
+let shipForDrop = null;
+
+for(const container of containers) {
+    if (container.id.includes('right')) {
+        container.addEventListener("dragover", dragover);
+        container.addEventListener("dragenter", dragenter);
+        container.addEventListener("drop", drop);
+    }
+}
+
+for (const ship of ships) {
+    ship.addEventListener('mousedown', onMouseDown);
+}
+
+function onMouseDown(e) {
+    shipForDrop = e.target;
+}
+
+function dragover(e) {
+    e.preventDefault();
+}
+
+function dragenter(e) {
+    e.preventDefault();
+}
+
+function drop() {
+    this.append(shipForDrop);
+    shipForDrop = null;
+}
+
 function handler(event) {
 
     const target = event.target;
